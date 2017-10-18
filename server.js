5fab0a15319a5abbec6c164444a61a662a3982fa
Ryan Edwards <ryan.deland.edwards@gmail.com>
2017-10-18 14:38:48 -0400

    add a semicolon here

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ deck = shuffleDeck();
 function createDeck() {
   let suits = ['spades', 'clubs', 'hearts', 'diamonds'];
   let values = ['A','2','3','4','5','6','7','8','9','10','J','Q','K'];
-  let realValues = [1,2,3,4,5,6,7,8,9,10,10,10,10]
+  let realValues = [1,2,3,4,5,6,7,8,9,10,10,10,10];
 
   suits.forEach((suit) => {
     values.forEach((value, index) => {
